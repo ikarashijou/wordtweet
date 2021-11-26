5cb4863b020f79d90ffece3f51ffc8f73b293fa8
ikarashijou <mancapitalikarashi@gmail.com>
2021-11-26 22:38:07 +0900

    データベースエラー対応

--- DB.sql
+++ DB.sql
@@ -19,58 +19,72 @@ SET time_zone = "+00:00";
 --
 -- テーブルの構造 `admin`
 --
-CREATE TABLE `admin` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `created` datetime NULL DEFAULT NULL,
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE admin (
+  id int AUTO_INCREMENT NOT NULL,
+  name varchar(255) NOT NULL,
+  password varchar(255) NOT NULL,
+  created datetime NULL DEFAULT NULL,
+  PRIMARY KEY (id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 --
 -- テーブルのデータのダンプ `admin`
 --
 
-INSERT INTO `admin` (`id`, `name`,'pass') VALUES
-(1, 'admin','pass'),
+INSERT INTO admin (id, name, password) VALUES
+(1, 'admin','pass');
 -- --------------------------------------------------------
 
 --
 -- テーブルの構造 `users`
 --
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `created` datetime NULL DEFAULT NULL,
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
---
--- テーブルのデータのダンプ `users`
---
+CREATE TABLE users (
+  id int AUTO_INCREMENT NOT NULL,
+  name varchar(255) NOT NULL,
+  password varchar(255) NOT NULL,
+  created datetime NULL DEFAULT NULL,
+  PRIMARY KEY (id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `users` (`id`, `name`,'pass') VALUES
-(1, 'user','pass'),
--- --------------------------------------------------------
 
 --
 -- テーブルの構造 `shugoData`
 --
 
-CREATE TABLE `shugoData` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  'text' varchar(255) NOT NULL,
-  `created` datetime NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE shugoData (
+  id int AUTO_INCREMENT NOT NULL,
+  user_id int NOT NULL,
+  text varchar(255) NOT NULL,
+  created datetime NULL DEFAULT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- テーブルのデータのダンプ `shugoData`
+--
+
+INSERT INTO shugoData (user_id, text) VALUES
+(1,'私は');
+-- --------------------------------------------------------
 
 
 --
 -- テーブルの構造 `jutugoData`
 --
 
-CREATE TABLE `shugoData` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  'text' varchar(255) NOT NULL,
-  `created` datetime NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE jutugoData (
+  id int AUTO_INCREMENT NOT NULL,
+  user_id int NOT NULL,
+  text varchar(255) NOT NULL,
+  created datetime NULL DEFAULT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- テーブルのデータのダンプ `jutugoData`
+--
+
+INSERT INTO jutugoData (user_id, text) VALUES
+(1,'楽しい');
+-- --------------------------------------------------------
 
 
